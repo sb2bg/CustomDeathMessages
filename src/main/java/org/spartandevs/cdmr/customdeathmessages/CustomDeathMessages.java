a70ff35ae7e1217c56a0ae91d4162f8dee78f1c7
sb2bg <sullivanbognar@gmail.com>
2022-11-09 21:45:59 -0600

    Initial commit

---
+++ src/main/java/org/spartandevs/cdmr/customdeathmessages/CustomDeathMessages.java
@@ -0,0 +1,16 @@
+package org.spartandevs.cdmr.customdeathmessages;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomDeathMessages extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
