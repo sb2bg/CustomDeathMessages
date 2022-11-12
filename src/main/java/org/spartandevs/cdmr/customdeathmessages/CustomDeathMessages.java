48d205020d47e6d9f6d5b2f69f819e6bb4060785
sb2bg <sullivanbognar@gmail.com>
2022-11-11 21:15:43 -0600

    Config manager getter

--- src/main/java/org/spartandevs/cdmr/customdeathmessages/CustomDeathMessages.java
+++ src/main/java/org/spartandevs/cdmr/customdeathmessages/CustomDeathMessages.java
@@ -1,16 +1,21 @@
 package org.spartandevs.cdmr.customdeathmessages;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.spartandevs.cdmr.customdeathmessages.util.ConfigManager;
 
 public final class CustomDeathMessages extends JavaPlugin {
+    private final ConfigManager configManager = new ConfigManager(this);
 
     @Override
     public void onEnable() {
-
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    public ConfigManager getConfigManager() {
+        return configManager;
     }
 }
