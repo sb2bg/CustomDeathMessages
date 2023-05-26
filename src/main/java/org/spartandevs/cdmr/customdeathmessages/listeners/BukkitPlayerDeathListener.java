add6c18b5dd64a678b06ea43781a8b4113bf8e43
sb2bg <sullivanbognar@gmail.com>
2023-05-26 13:39:53 -0500

    Call custom death listener on player death

--- src/main/java/org/spartandevs/cdmr/customdeathmessages/listeners/BukkitPlayerDeathListener.java
+++ src/main/java/org/spartandevs/cdmr/customdeathmessages/listeners/BukkitPlayerDeathListener.java
@@ -1,4 +1,33 @@
 package org.spartandevs.cdmr.customdeathmessages.listeners;
 
-public class BukkitPlayerDeathListener {
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.spartandevs.cdmr.customdeathmessages.CustomDeathMessages;
+import org.spartandevs.cdmr.customdeathmessages.events.CustomPlayerDeathEvent;
+import org.spartandevs.cdmr.customdeathmessages.util.DeathCause;
+
+public class BukkitPlayerDeathListener implements Listener {
+    private final CustomDeathMessages plugin;
+
+    public BukkitPlayerDeathListener(CustomDeathMessages plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Player victim = event.getEntity();
+        Player killer = victim.getKiller();
+
+        if (victim.getLastDamageCause() == null) {
+            return;
+        }
+
+        DeathCause deathCause = DeathCause.fromDamageCause(victim.getLastDamageCause().getCause());
+        CustomPlayerDeathEvent customPlayerDeathEvent = new CustomPlayerDeathEvent(
+                deathCause, killer, victim, event::setDeathMessage);
+
+        plugin.getServer().getPluginManager().callEvent(customPlayerDeathEvent);
+    }
 }
