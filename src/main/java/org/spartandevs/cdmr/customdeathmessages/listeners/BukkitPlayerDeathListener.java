238f357f02f27e8b796a37db47651dd565f7986f
sb2bg <sullivanbognar@gmail.com>
2023-05-27 02:18:17 -0500

    Change setDeathMessage to accept DeathMessages

--- src/main/java/org/spartandevs/cdmr/customdeathmessages/listeners/BukkitPlayerDeathListener.java
+++ src/main/java/org/spartandevs/cdmr/customdeathmessages/listeners/BukkitPlayerDeathListener.java
@@ -5,11 +5,18 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.spartandevs.cdmr.customdeathmessages.CustomDeathMessages;
+import org.spartandevs.cdmr.customdeathmessages.chat.DeathMessage;
 import org.spartandevs.cdmr.customdeathmessages.events.CustomPlayerDeathEvent;
 import org.spartandevs.cdmr.customdeathmessages.util.DeathCause;
 
 public class BukkitPlayerDeathListener implements Listener {
+    public interface OriginalDeathMessageSetter {
+        void setDeathMessage(String message);
+    }
+
     private final CustomDeathMessages plugin;
+
+    private OriginalDeathMessageSetter deathMessageSetter;
 
     public BukkitPlayerDeathListener(CustomDeathMessages plugin) {
         this.plugin = plugin;
@@ -25,9 +32,22 @@ public class BukkitPlayerDeathListener implements Listener {
         }
 
         DeathCause deathCause = DeathCause.fromDamageCause(victim.getLastDamageCause().getCause());
+
+        deathMessageSetter = event::setDeathMessage;
+
         CustomPlayerDeathEvent customPlayerDeathEvent = new CustomPlayerDeathEvent(
-                deathCause, killer, victim, event::setDeathMessage);
+                deathCause, killer, victim, this::setDeathMessage);
 
         plugin.getServer().getPluginManager().callEvent(customPlayerDeathEvent);
+    }
+
+    private void setDeathMessage(DeathMessage deathMessage) {
+        switch (deathMessage.getMessageType()) {
+            case STRING -> deathMessageSetter.setDeathMessage(deathMessage.getStringMessage());
+            case JSON -> {
+                deathMessageSetter.setDeathMessage("");
+                plugin.getServer().spigot().broadcast(deathMessage.getTextComponent());
+            }
+        }
     }
 }
