777197353d0d972589541c13c555b52b9418c4ad
sb2bg <sullivanbognar@gmail.com>
2023-05-27 20:30:21 -0500

    Accept upstream error messages

--- src/main/java/org/spartandevs/customdeathmessages/listeners/CustomPlayerDeathListener.java
+++ src/main/java/org/spartandevs/customdeathmessages/listeners/CustomPlayerDeathListener.java
@@ -6,12 +6,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
-import org.spartandevs.cdmr.customdeathmessages.CustomDeathMessages;
-import org.spartandevs.cdmr.customdeathmessages.chat.DeathMessage;
-import org.spartandevs.cdmr.customdeathmessages.chat.JsonTransforms;
-import org.spartandevs.cdmr.customdeathmessages.chat.PlaceholderPopulator;
-import org.spartandevs.cdmr.customdeathmessages.events.CustomPlayerDeathEvent;
-import org.spartandevs.cdmr.customdeathmessages.util.ConfigManager;
+import org.spartandevs.customdeathmessages.CustomDeathMessages;
+import org.spartandevs.customdeathmessages.chat.DeathMessage;
+import org.spartandevs.customdeathmessages.chat.JsonTransforms;
+import org.spartandevs.customdeathmessages.chat.PlaceholderPopulator;
+import org.spartandevs.customdeathmessages.events.CustomPlayerDeathEvent;
+import org.spartandevs.customdeathmessages.util.ConfigManager;
+import org.spartandevs.customdeathmessages.util.DeathCause;
 
 import java.util.Objects;
 
@@ -26,7 +27,6 @@ public class CustomPlayerDeathListener implements Listener {
     public void onPlayerDeath(CustomPlayerDeathEvent event) {
         ConfigManager config = plugin.getConfigManager();
 
-        JsonTransforms jsonTransforms = JsonTransforms.getTransforms(plugin);
         PlaceholderPopulator populator;
         ItemStack weapon = null;
 
@@ -34,10 +34,15 @@ public class CustomPlayerDeathListener implements Listener {
             populator = new PlaceholderPopulator(plugin, event.getVictim());
         } else if (event.getKiller() instanceof Player killer) {
             weapon = getKillWeapon(killer);
-            populator = new PlaceholderPopulator(plugin, event.getVictim(), event.getKiller(), weapon);
+
+            populator = config.isItemOnHoverEnabled()
+                    ? new PlaceholderPopulator(plugin, event.getVictim(), event.getKiller())
+                    : new PlaceholderPopulator(plugin, event.getVictim(), event.getKiller(), weapon);
         } else {
             populator = new PlaceholderPopulator(plugin, event.getVictim(), event.getKiller());
         }
+
+        JsonTransforms jsonTransforms = new JsonTransforms(plugin, event.getOriginalDeathMessage(), weapon);
 
         if (config.doLightningStrike()) {
             event.getVictim().getWorld().strikeLightningEffect(event.getVictim().getLocation());
@@ -55,31 +60,33 @@ public class CustomPlayerDeathListener implements Listener {
         }
 
         if (config.isGlobalMessageEnabled()) {
+            DeathCause propagated = plugin.getMessagePropagator().getDeathMessage(event.getVictim().getUniqueId());
+
             DeathMessageResolver resolver = weapon != null && weapon.getType() == Material.AIR
                     ? config::getMeleeMessage
-                    : () -> config.getMessage(event.getDeathCause());
+                    : (propagated == null
+                    ? () -> config.getMessage(event.getDeathCause())
+                    : () -> config.getMessage(propagated));
 
-            String message = resolver.getStrMessage();
+            String message = resolver.getMessage();
 
             if (message == null) {
                 return;
             }
 
-            event.setDeathMessage(new DeathMessage(message, populator, jsonTransforms));
+            DeathMessage.MessageType type = config.isItemOnHoverEnabled() || config.isOriginalOnHoverEnabled()
+                    ? DeathMessage.MessageType.JSON
+                    : DeathMessage.MessageType.STRING;
+
+            event.setDeathMessage(new DeathMessage(message, populator, type), jsonTransforms);
         }
     }
 
     interface DeathMessageResolver {
-        String getStrMessage();
+        String getMessage();
     }
 
     private static ItemStack getKillWeapon(Player killer) {
-        ItemStack killWeapon = killer.getInventory().getItemInMainHand();
-
-        if (killWeapon.getType() == Material.AIR) {
-            return null;
-        }
-
-        return killWeapon;
+        return killer.getInventory().getItemInMainHand();
     }
 }
