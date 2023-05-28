d2c20addb41fb5d2f326e00fd0b2b895bf5f3849
sb2bg <sullivanbognar@gmail.com>
2023-05-27 20:29:50 -0500

    Add MessagePropagator

--- src/main/java/org/spartandevs/customdeathmessages/CustomDeathMessages.java
+++ src/main/java/org/spartandevs/customdeathmessages/CustomDeathMessages.java
@@ -1,18 +1,18 @@
 package org.spartandevs.customdeathmessages;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import org.spartandevs.cdmr.customdeathmessages.chat.ChatColor;
-import org.spartandevs.cdmr.customdeathmessages.listeners.BukkitKilledByEntityListener;
-import org.spartandevs.cdmr.customdeathmessages.listeners.BukkitLoginListener;
-import org.spartandevs.cdmr.customdeathmessages.listeners.BukkitPlayerDeathListener;
-import org.spartandevs.cdmr.customdeathmessages.listeners.CustomPlayerDeathListener;
-import org.spartandevs.cdmr.customdeathmessages.util.ConfigManager;
-import org.spartandevs.cdmr.customdeathmessages.util.ServerVersion;
+import org.spartandevs.customdeathmessages.chat.ChatColor;
+import org.spartandevs.customdeathmessages.listeners.BukkitKilledByEntityListener;
+import org.spartandevs.customdeathmessages.listeners.BukkitLoginListener;
+import org.spartandevs.customdeathmessages.listeners.BukkitPlayerDeathListener;
+import org.spartandevs.customdeathmessages.listeners.CustomPlayerDeathListener;
+import org.spartandevs.customdeathmessages.util.ConfigManager;
+import org.spartandevs.customdeathmessages.util.MessagePropagator;
 
 public final class CustomDeathMessages extends JavaPlugin {
-    private final ServerVersion serverVersion = ServerVersion.getServerVersion();
     private final ConfigManager configManager = new ConfigManager(this);
     private final ChatColor chatColor = new ChatColor(this);
+    private final MessagePropagator messagePropagator = new MessagePropagator(this);
 
     @Override
     public void onEnable() {
@@ -23,24 +23,24 @@ public final class CustomDeathMessages extends JavaPlugin {
     private void registerEvents() {
         getServer().getPluginManager().registerEvents(new BukkitLoginListener(this), this);
         getServer().getPluginManager().registerEvents(new BukkitPlayerDeathListener(this), this);
-        getServer().getPluginManager().registerEvents(new BukkitKilledByEntityListener(), this);
+        getServer().getPluginManager().registerEvents(new BukkitKilledByEntityListener(this), this);
         getServer().getPluginManager().registerEvents(new CustomPlayerDeathListener(this), this);
-    }
-
-    @Override
-    public void onDisable() {
-
     }
 
     public ConfigManager getConfigManager() {
         return configManager;
     }
-
-    public ServerVersion getServerVersion() {
-        return serverVersion;
-    }
-
+    
     public String translateColorCodes(String message) {
         return chatColor.translateAlternateColorCodes(message);
+    }
+
+    public void reload() {
+        configManager.reloadConfig();
+        messagePropagator.clear();
+    }
+
+    public MessagePropagator getMessagePropagator() {
+        return messagePropagator;
     }
 }
