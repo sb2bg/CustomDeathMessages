0956d2d6a100455424b8479e2e455ce8130505a7
sb2bg <sullivanbognar@gmail.com>
2023-05-27 20:31:37 -0500

    DeathMessage holder to translate messages

---
+++ src/main/java/org/spartandevs/customdeathmessages/chat/DeathMessage.java
@@ -0,0 +1,32 @@
+package org.spartandevs.customdeathmessages.chat;
+
+import net.md_5.bungee.api.chat.TextComponent;
+
+public class DeathMessage {
+    private final PlaceholderPopulator populator;
+    private String message;
+    private MessageType messageType;
+
+    public DeathMessage(String message, PlaceholderPopulator populator, MessageType messageType) {
+        this.populator = populator;
+        this.message = message;
+        this.messageType = messageType;
+    }
+
+    public String getStringMessage() {
+        return populator.replace(message);
+    }
+
+    public TextComponent getTextComponent(JsonTransforms transforms) {
+        return transforms.getTransformers() == JsonTransformers.NONE ? null : transforms.transform(getStringMessage());
+    }
+
+    public enum MessageType {
+        STRING,
+        JSON
+    }
+
+    public MessageType getMessageType() {
+        return messageType;
+    }
+}
