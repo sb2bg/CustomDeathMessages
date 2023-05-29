6e14e1bd06494a95cc74fb567d8848f49f914aac
sb2bg <sullivanbognar@gmail.com>
2023-05-29 05:15:28 -0500

    Add Plugin dependency

--- src/main/java/org/spartandevs/customdeathmessages/commands/CDMBaseCommand.java
+++ src/main/java/org/spartandevs/customdeathmessages/commands/CDMBaseCommand.java
@@ -1,15 +1,19 @@
 package org.spartandevs.customdeathmessages.commands;
 
 import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.Dependency;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.spartandevs.customdeathmessages.CustomDeathMessages;
 
 public class CDMBaseCommand extends BaseCommand {
+    @Dependency
+    protected CustomDeathMessages plugin;
     protected static final String PERMISSION_PREFIX = "customdeathmessages.";
     protected static final String PERMISSION_RELOAD = PERMISSION_PREFIX + "reload";
     protected static final String PERMISSION_MODIFY = PERMISSION_PREFIX + "modify";
     protected static final String PERMISSION_ADMIN = PERMISSION_PREFIX + "admin";
-    private static final String MSG_PREFIX = "&8[&cCDM&8] ";
+    private static final String MSG_PREFIX = "&8&l[&e&lCDM&8&l] ";
 
     protected static void sendMessage(CommandSender sender, String message) {
         sender.sendMessage(ChatColor.translateAlternateColorCodes('&', MSG_PREFIX + message));
