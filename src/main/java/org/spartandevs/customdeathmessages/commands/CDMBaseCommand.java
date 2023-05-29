13da7dd738a2802f7221c14a33b290de5c12d8f4
sb2bg <sullivanbognar@gmail.com>
2023-05-29 06:51:56 -0500

    Use instanced ChatColor

--- src/main/java/org/spartandevs/customdeathmessages/commands/CDMBaseCommand.java
+++ src/main/java/org/spartandevs/customdeathmessages/commands/CDMBaseCommand.java
@@ -2,7 +2,6 @@ package org.spartandevs.customdeathmessages.commands;
 
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.Dependency;
-import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.spartandevs.customdeathmessages.CustomDeathMessages;
 
@@ -10,12 +9,9 @@ public class CDMBaseCommand extends BaseCommand {
     @Dependency
     protected CustomDeathMessages plugin;
     protected static final String PERMISSION_PREFIX = "customdeathmessages.";
-    protected static final String PERMISSION_RELOAD = PERMISSION_PREFIX + "reload";
-    protected static final String PERMISSION_MODIFY = PERMISSION_PREFIX + "modify";
-    protected static final String PERMISSION_ADMIN = PERMISSION_PREFIX + "admin";
     private static final String MSG_PREFIX = "&8&l[&e&lCDM&8&l] ";
 
-    protected static void sendMessage(CommandSender sender, String message) {
-        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', MSG_PREFIX + message));
+    protected void sendMessage(CommandSender sender, String message) {
+        sender.sendMessage(plugin.translateColorCodes(MSG_PREFIX + message));
     }
 }
