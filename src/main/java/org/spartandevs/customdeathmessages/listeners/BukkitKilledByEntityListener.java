170a779a0a15769fb2f1422621894a836675a454
sb2bg <sullivanbognar@gmail.com>
2023-06-15 17:15:46 -0500

    Avoid hearts from displaying in chat by McMMO

--- src/main/java/org/spartandevs/customdeathmessages/listeners/BukkitKilledByEntityListener.java
+++ src/main/java/org/spartandevs/customdeathmessages/listeners/BukkitKilledByEntityListener.java
@@ -61,7 +61,8 @@ public class BukkitKilledByEntityListener implements Listener {
             deathCause = DeathCause.WITHER_BOSS;
         }
 
-        if (entity.getCustomName() != null) {
+        // Avoid ❤ in custom named entities (McMMO)
+        if (entity.getCustomName() != null && !entity.getCustomName().contains("❤")) {
             deathCause = DeathCause.CUSTOM_NAMED_ENTITY;
         }
 
