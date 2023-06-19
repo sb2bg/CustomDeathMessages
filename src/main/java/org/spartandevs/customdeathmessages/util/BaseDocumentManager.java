48273f94f362963eba153aa9f0c87f68df456580
sb2bg <sullivanbognar@gmail.com>
2023-06-19 15:09:05 -0500

    Change BaseDocumentManager to handle more logic

--- src/main/java/org/spartandevs/customdeathmessages/util/BaseDocumentManager.java
+++ src/main/java/org/spartandevs/customdeathmessages/util/BaseDocumentManager.java
@@ -1,32 +1,82 @@
 package org.spartandevs.customdeathmessages.util;
 
 import dev.dejvokep.boostedyaml.YamlDocument;
+import dev.dejvokep.boostedyaml.settings.dumper.DumperSettings;
+import dev.dejvokep.boostedyaml.settings.general.GeneralSettings;
+import dev.dejvokep.boostedyaml.settings.loader.LoaderSettings;
+import dev.dejvokep.boostedyaml.settings.updater.UpdaterSettings;
 import org.spartandevs.customdeathmessages.CustomDeathMessages;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 public abstract class BaseDocumentManager {
     protected final CustomDeathMessages plugin;
-    protected final YamlDocument document;
+    protected YamlDocument document;
+    private final String resource;
 
-    public BaseDocumentManager(CustomDeathMessages plugin) {
+    public BaseDocumentManager(CustomDeathMessages plugin, String resource) {
         this.plugin = plugin;
-        this.document = getDocument();
+        this.resource = resource;
+        loadDocument();
     }
 
-    protected abstract YamlDocument getDocument();
+    protected void loadDocument() {
+        InputStream resourceStream = plugin.getResource(resource);
+
+        if (resourceStream == null) {
+            errorAndDisable("Could not find resource " + resource);
+            return;
+        }
+
+        if (!sneaky(() -> document = YamlDocument.create(
+                new File(plugin.getDataFolder(), resource),
+                resourceStream,
+                GeneralSettings.DEFAULT,
+                LoaderSettings.builder().setAutoUpdate(true).build(),
+                DumperSettings.DEFAULT,
+                updater()))) {
+            errorAndDisable("Could not load document " + resource);
+        }
+    }
+
+    protected UpdaterSettings updater() {
+        return UpdaterSettings.DEFAULT;
+    }
 
     interface ConfigAction {
         void run() throws IOException;
     }
 
-    protected boolean sneaky(ConfigManager.ConfigAction action) {
+    protected boolean sneaky(ConfigAction action) {
         try {
             action.run();
             return true;
         } catch (IOException e) {
-            plugin.getLogger().warning("Unable to save config.yml, changes will not take effect.");
             return false;
         }
     }
+
+    protected void save() {
+        if (sneaky(document::save)) {
+            return;
+        }
+
+        plugin.getLogger().severe("Could not save " + resource);
+    }
+
+    public void reload() {
+        if (sneaky(document::reload)) {
+            return;
+        }
+
+        plugin.getLogger().severe("Could not reload " + resource);
+    }
+
+    protected void errorAndDisable(String message) {
+        plugin.getLogger().severe(message);
+        plugin.getServer().getPluginManager().disablePlugin(plugin);
+    }
 }
+
