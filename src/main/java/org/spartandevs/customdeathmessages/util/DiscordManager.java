7be7e0b4a2c1626fefa41a207021b167ebb16e10
sb2bg <sullivanbognar@gmail.com>
2023-06-24 22:14:34 -0500

    First commit

---
+++ src/main/java/org/spartandevs/customdeathmessages/util/DiscordManager.java
@@ -0,0 +1,49 @@
+package org.spartandevs.customdeathmessages.util;
+
+import net.essentialsx.api.v2.services.discord.DiscordService;
+import net.essentialsx.api.v2.services.discord.MessageType;
+import org.spartandevs.customdeathmessages.CustomDeathMessages;
+
+public class DiscordManager {
+    private final CustomDeathMessages plugin;
+    private DiscordInterface discord;
+
+    public DiscordManager(CustomDeathMessages plugin) {
+        this.plugin = plugin;
+
+        if (plugin.getServer().getPluginManager().isPluginEnabled("EssentialsDiscord")) {
+            DiscordService essDiscord = plugin.getServer().getServicesManager().load(DiscordService.class);
+
+            if (essDiscord == null) {
+                plugin.getLogger().warning("Failed to load EssentialsDiscord integration.");
+                return;
+            }
+
+            discord = message -> {
+                try {
+                    essDiscord.sendMessage(MessageType.DefaultTypes.DEATH, message, true);
+                } catch (Exception e) {
+                    plugin.getLogger().warning("Failed to send message to Discord.");
+                }
+            };
+
+            plugin.getLogger().info("Successfully loaded EssentialsDiscord support.");
+        } else if (plugin.getServer().getPluginManager().isPluginEnabled("DiscordSRV")) {
+            plugin.getLogger().warning("DiscordSRV support is not yet implemented.");
+        }
+    }
+
+    public void sendDeathMessage(String message) {
+        if (discord != null) {
+            discord.sendDeathMessage(message);
+        }
+    }
+
+    public boolean isDiscordEnabled() {
+        return discord != null;
+    }
+}
+
+interface DiscordInterface {
+    void sendDeathMessage(String message);
+}
