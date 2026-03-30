45000b375fac7519e53cbe48b3f3b5ea8de96415
sb2bg <sullivanbognar@gmail.com>
2026-03-29 20:17:49 -0500

    Update item hover code

--- src/main/java/org/spartandevs/customdeathmessages/chat/ItemSerializer.java
+++ src/main/java/org/spartandevs/customdeathmessages/chat/ItemSerializer.java
@@ -7,15 +7,45 @@ import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Method;
+import java.util.Locale;
+
 public class ItemSerializer {
     @SuppressWarnings("deprecation")
     public static BaseComponent[] serializeItemStack(ItemStack itemStack) {
         String nbt = NBTEditor.getNBTCompound(itemStack).toString();
-        HoverEvent event = new HoverEvent(HoverEvent.Action.SHOW_ITEM, new BaseComponent[]{new TextComponent(nbt)});
+        HoverEvent event = createItemHoverEvent(itemStack, nbt);
         BaseComponent[] component = HoverTransformers.createBaseComponent(getItemName(itemStack));
         HoverTransformers.setHoverEvent(component, event);
 
         return component;
+    }
+
+    @SuppressWarnings("deprecation")
+    private static HoverEvent createItemHoverEvent(ItemStack itemStack, String nbt) {
+        try {
+            Class<?> contentClass = Class.forName("net.md_5.bungee.api.chat.hover.content.Content");
+            Class<?> itemClass = Class.forName("net.md_5.bungee.api.chat.hover.content.Item");
+            Class<?> itemTagClass = Class.forName("net.md_5.bungee.api.chat.ItemTag");
+            Method ofNbt = itemTagClass.getMethod("ofNbt", String.class);
+            Object itemTag = ofNbt.invoke(null, nbt);
+            Object hoverItem = itemClass
+                    .getConstructor(String.class, int.class, itemTagClass)
+                    .newInstance(getItemId(itemStack), itemStack.getAmount(), itemTag);
+            Object contents = Array.newInstance(contentClass, 1);
+            Array.set(contents, 0, hoverItem);
+
+            return HoverEvent.class
+                    .getConstructor(HoverEvent.Action.class, contents.getClass())
+                    .newInstance(HoverEvent.Action.SHOW_ITEM, contents);
+        } catch (ReflectiveOperationException ignored) {
+            return new HoverEvent(HoverEvent.Action.SHOW_ITEM, new BaseComponent[]{new TextComponent(nbt)});
+        }
+    }
+
+    private static String getItemId(ItemStack itemStack) {
+        return "minecraft:" + itemStack.getType().name().toLowerCase(Locale.ROOT);
     }
 
     public static String getItemName(ItemStack item) {
